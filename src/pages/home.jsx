31302dbaef54c85a83da4b51f1949f4075012975
digomattar21 <digomattar@gmail.com>
2021-04-10 17:42:39 -0200

    fixed bug of css crash

---
+++ src/pages/home.jsx
@@ -0,0 +1,57 @@
+import { SportsList } from '../components/SportsList';
+import { NavBar } from '../components/NavBar';
+import { RightSideNavAuth } from '../components/RightSideNavAuth';
+import { Grid } from '@material-ui/core';
+import { useContext, useEffect, useState } from 'react';
+import SportsMiddleSection from '../components/SportsMiddleSection/SportsMiddleSection';
+import AuthContext from '../context/UserProvider/context';
+import {RightSideNav} from '../components/RightSideNav';
+import Api from '../utils/api.utils'
+import IndexMiddleSectionNav from '../components/IndexMiddleSectionNav';
+import IndexMiddleSection from '../components/IndexMiddleSection';
+
+
+
+function Home2({sports}) {
+    const {userAuth,changeUserAuth} = useContext(AuthContext);
+
+    return (
+        <>
+          <NavBar />
+          
+            <Grid container spacing={3}>
+              <SportsList sports={sports}/>
+              <Grid item xs={6}>
+              <IndexMiddleSectionNav/>
+              <IndexMiddleSection/>
+              </Grid>
+            {userAuth && <RightSideNavAuth />}
+            {!userAuth && <RightSideNav />}
+            </Grid>
+          
+        </>
+      );
+}
+
+export async function getStaticProps(){
+  
+    try {
+      let req = await Api.getSports()
+      
+      const sports = req.data;
+        return {
+          props:{
+            sports
+          }
+        }
+       
+      
+    } catch (error) {
+      console.log(error.message)
+      
+    }
+  
+  
+  }
+
+export default Home2;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,58 +1,38 @@
-import styled from 'styled-components';
-import { SportsList } from '../components/SportsList';
-import { NavBar } from '../components/NavBar';
-import { RightSideNavAuth } from '../components/RightSideNavAuth';
-import { Grid } from '@material-ui/core';
 import { useContext, useEffect, useState } from 'react';
-import SportsMiddleSection from '../components/SportsMiddleSection/SportsMiddleSection';
 import AuthContext from '../context/UserProvider/context';
-import {RightSideNav} from '../components/RightSideNav';
-import Api from '../utils/api.utils'
-import IndexMiddleSectionNav from '../components/IndexMiddleSectionNav';
-import IndexMiddleSection from '../components/IndexMiddleSection';
+import CachedIcon from "@material-ui/icons/Cached";
+import { useRouter } from 'next/router';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  cacheIcon: {
+    fontSize: "25px",
+    color: "#D6ED17FF",
+    animation: "$spin 3s infinite",
+  },
+}))
 
 
-function Home({sports}) {
-  const {userAuth,changeUserAuth} = useContext(AuthContext)
+function Home() {
+  const {userAuth,changeUserAuth} = useContext(AuthContext);
+  const [loading, setLoading] = useState(true)
+  const router = useRouter();
+  const classes = useStyles();
+
+  useEffect(()=>{
+    router.push('/home')
+  },[])
 
   return (
     <>
-      <NavBar />
-      
-        <Grid container spacing={3}>
-          <SportsList sports={sports}/>
-          <Grid item xs={6}>
-          <IndexMiddleSectionNav/>
-          <IndexMiddleSection/>
-          </Grid>
-        {userAuth && <RightSideNavAuth />}
-        {!userAuth && <RightSideNav />}
-        </Grid>
-      
+      {loading && 
+        <CachedIcon className={classes.cachedIcon}/>
+      }
     </>
   );
 }
 
-export async function getStaticProps(){
-  
-  try {
-    let req = await Api.getSports()
-    
-    const sports = req.data;
-      return {
-        props:{
-          sports
-        }
-      }
-     
-    
-  } catch (error) {
-    console.log(error.message)
-    
-  }
 
-
-}
 
 
 
